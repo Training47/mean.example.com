c289f38ea3b4f76e41ef08d71296f77cf5153346
Lisa Pena <lisapena13@gmail.com>
2020-08-25 14:36:46 -0500

    Add logout methods

--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,15 @@ router.get('/', function(req, res, next) {
   res.render('auth/index', { title: 'User Authentication' });
 });
 
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/auth');
+});
+
+router.get('/logout', function(req, res){
+    console.log(req.session);
+    req.logout();
+    console.log(req.session);
+  });
+
 module.exports = router;
