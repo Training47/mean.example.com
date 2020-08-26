c53e7de058a02fea6015b7fdc3225af1921f5f3e
Lisa Pena <lisapena13@gmail.com>
2020-08-26 17:48:58 -0500

    BuildViewDeleteUser

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -283,9 +283,8 @@ function deleteView(id){
 
         <div class="text-center">
           <br>
-          <a class="btn btn-lg btn-danger text-white">
-            Yes delete ${data.user.username}
-          </a>
+          <a onclick="usersApp.deleteUser('${data.user._id}');" class="btn btn-lg btn-danger text-white">
+          Yes delete ${data.user.username}</a>
         </div>
 
       </div>
@@ -293,6 +292,31 @@ function deleteView(id){
 
     app.innerHTML = card;
   }
+}
+
+function deleteUser(id){
+
+  let uri = `${window.location.origin}/api/users/${id}`;
+  let xhr = new XMLHttpRequest();
+  xhr.open('DELETE', uri);
+
+  xhr.setRequestHeader(
+    'Content-Type',
+    'application/json; charset=UTF-8'
+  );
+
+  xhr.send();
+
+  xhr.onload = function(){
+    let data = JSON.parse(xhr.response);
+    if(data.success === true){
+      window.location.hash = '#';
+    }else{
+      alert('Unknown error, the user could not be deleted');
+    }
+
+  }
+
 }
 
 return {
@@ -327,6 +351,10 @@ return {
           viewUsers();
           break;
             }
+          },
+
+          deleteUser: function(id){
+            deleteUser(id);
           }
 
         }
