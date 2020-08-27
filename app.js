b814bfca1aa6291549b464929613f4f48c3ce2c9
Lisa Pena <lisapena13@gmail.com>
2020-08-27 11:06:15 -0500

    Implement a route for api/articles

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var authRouter = require('./routes/auth');
 var usersRouter = require('./routes/users');
 var apiAuthRouter = require('./routes/api/auth');
 var apiUsersRouter = require('./routes/api/users');
+var apiArticlesRouter = require('./routes/api/articles');
 
 
 var app = express();
@@ -124,6 +125,7 @@ app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 app.use('/api/users', apiUsersRouter);
+app.use('/api/articles', apiArticlesRouter);
 app.use('/api/auth', apiAuthRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
