d5a569da43b195bae637dc38baab9cf1c18342c0
Lisa Pena <lisapena13@gmail.com>
2020-08-27 11:41:23 -0500

    Updated articles.js

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var Users = require('../../models/articles');
+var Articles = require('../../models/articles');
 
 router.get('/', function(req, res, next) {
-  Users.find({},function(err, articles){
+  Articles.find({},function(err, articles){
     if(err){
      return res.json({'success':false, 'error': err});
    }
@@ -11,15 +11,96 @@ router.get('/', function(req, res, next) {
   });
 });
 
+
 router.get('/:articleId', function(req,res){
   
-    var userId = req.params.articleId;
-     Users.findOne({'_id':articleId}, function(err, article){
-       if(err){
-        return res.json({'success':false, 'error': err});
-      }
-       return res.json({'success':true, 'article': article});
-     });
+  var articleId = req.params.articleId;
+   Articles.findOne({'_id':articleId}, function(err, article){
+     if(err){
+      return res.json({'success':false, 'error': err});
+    }
+     return res.json({'success':true, 'article': article});
    });
+ });
+
+ router.post('/', function(req, res) {
+  Articles.create(new Articles({
+    title: req.body.titlename,
+    description: req.body.description,
+    keywords: req.body.keywords,
+    body: req.body.body,
+    published: req.body.published
+  }), function(err, article){
+    
+    if(err){
+      return res.json({success: false, article: req.body, error: err});
+    }
+
+    return res.json({success: true, article: article});
+    
+  });
+});
+
+router.put('/', function(req, res){
+
+  Articles.findOne({'_id': req.body._id}, function(err, article){
+
+   if(err) {
+     return res.json({success: false, error: err});
+   }
+
+   if(article) {
+
+    let data = req.body;
+
+    if(data.titlename){
+      article.titlename = data.titlename;
+    };
+
+    if(data.description){
+    article.description = data.description;
+    };
+
+    if(data.keywords){
+    article.keywords = data.keywords;
+    };
+
+    if(data.body){
+    article.body = data.body;
+    };
+
+    if(data.published){
+        article.published = data.published;
+        };
+
+        article.save(function(err){
+      if(err){
+        return res.json({success: false, error: err});
+      }else{
+        return res.json({success: true, article:article});
+      }
+    });
+
+   }
+
+  });
+  
+});
+
+router.delete('/:articleId', function(req,res){
+
+  var articleId = req.params.articleId;
+
+  Articles.remove({'_id':articleId}, function(err,removed){
+
+    if(err){
+      return res.json({success: false, error: err});
+    }
+
+    return res.json({success: true, status: removed});
+
+  });
+
+});
 
 module.exports = router;
